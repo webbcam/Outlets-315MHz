cf37a2d224c03d762d90a0e37edcb92fe46a2020
webbcam <webb.c.92@gmail.com>
2015-05-30 13:44:50 -0400

    documented blink sketch

--- examples/blink/blink.ino
+++ examples/blink/blink.ino
@@ -1,24 +1,50 @@
-#include "Outlets.h"
+/*********************************************************************
+This is an example sketch that controls "Woods Wireless
+Remote Control Outlets" with a 315MHz RF Transmitter and Arduino
 
-ChannelF outlet(5);
+You can pick up a set of outlets here:   
+        https://amzn.com/B003ZTWYXY
+Along with a transmitter to talk to it:
+        http://amzn.com/B00LNADJS6
+
+Please see http://www.webbhacks.com/Outlets
+for more detailed information on this project
+
+Written by Cameron J. Webb  (cam@webbhacks.com)
+
+Copyright (c) 2015 Cameron J. Webb  
+MIT License, check LICENSE for more information
+All text above must be included in any redistribution
+
+**********************************************************************
+                         Simple Blink Sketch
+  This sketch will just cycle through each minor channel of the 
+  set of outlets turning each one on and then each one off
+*********************************************************************/
+
+
+#include "Outlets.h"  //  include all channels
+
+ChannelF outlet(5); //  make a new outlet instance with
+                    //  major channel F
 void setup() {
-  // put your setup code here, to run once:
+  //  nothing to set up
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  outlet.toggle(1, true);
+  //  turn each minor channel of outlet set on
+  outlet.toggle(1, true); //  turn ch1 on
   delay(1000);
-  outlet.toggle(2, true);
+  outlet.toggle(2, true); //  turn ch2 on
   delay(1000);
-  outlet.toggle(3, true);
+  outlet.toggle(3, true); //  turn ch3 on
   delay(500);
-  outlet.toggle(1, false);
+  outlet.toggle(1, false);  //  turn ch1 off
   delay(500);
-  outlet.toggle(2, false);
+  outlet.toggle(2, false);  //  turn ch2 off
   delay(500);
-  outlet.toggle(3, false);
+  outlet.toggle(3, false);  //  turn ch3 off
   delay(1000);
 
 }
